cc3fc5dd0d2ebe9f80d95f36e45085fddf17a734
pvwnthem <pvwn.them@gmail.com>
2023-07-08 21:25:47 -0400

    add Run to cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,17 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "gopwd",
 	Short: "A cli password manager written in go",
 	Long:  "gopwd is an encrypted cli password manager (similar to password-store) written in golang",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to gopwd!")
+	},
 }
