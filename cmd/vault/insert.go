996fa544c208d415bf37b26e0fe2a2a88f41134d
pvwnthem <pvwn.them@gmail.com>
2023-07-09 17:19:28 -0400

    take positonal args for insert

--- cmd/vault/insert.go
+++ cmd/vault/insert.go
@@ -1,17 +1,19 @@
 package vault
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var insertCmd = &cobra.Command{
-	Use:   "insert",
-	Short: "inserts a new password into the vault",
-	Long:  "",
+	Use:   "insert [site]",
+	Short: "Inserts a new password into the vault",
+	Args:  cobra.ExactArgs(1),
 
-	RunE: func(cmd *cobra.Command, args []string) error {
-
-		return nil
+	Run: func(cmd *cobra.Command, args []string) {
+		site := args[0]
+		fmt.Println("Inserting password for", site)
 	},
 }
 
