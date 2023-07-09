07300b7b06e3701cc4dff413c906a88f23c64624
pvwnthem <pvwn.them@gmail.com>
2023-07-08 22:27:01 -0400

    initialize vault

--- cmd/init.go
+++ cmd/init.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
 var (
 	Path string
+	Name string
 )
 
 var initCmd = &cobra.Command{
@@ -14,14 +18,31 @@ var initCmd = &cobra.Command{
 	Long:  "",
 
 	Run: func(cmd *cobra.Command, args []string) {
+		// Check if the vault already exists
+		if _, err := os.Stat(filepath.Join(Path, Name)); !os.IsNotExist(err) {
+			panic("Vault already exists")
+		}
 
+		err := os.MkdirAll(filepath.Join(Path, Name), 0755)
+		if err != nil {
+			panic(err)
+		}
 	},
 }
 
 func init() {
-	initCmd.Flags().StringVarP(&Path, "path", "p", "", "The path to create the vault at")
 
-	if err := initCmd.MarkFlagRequired("path"); err != nil {
+	homeDir, homeDirErr := os.UserHomeDir()
+
+	if homeDirErr != nil {
+		panic(homeDirErr)
+	}
+
+	initCmd.Flags().StringVarP(&Path, "path", "p", filepath.Join(homeDir, ".gopwd"), "The path to create the vault at")
+	initCmd.Flags().StringVarP(&Name, "name", "n", "", "The name of the vault")
+
+	err := initCmd.MarkFlagRequired("name")
+	if err != nil {
 		panic(err)
 	}
 
